3c3643c8ff396bb8277e153502cd6f96f48b098c
TofflerDev <baraban.ms@gmail.com>
2017-03-23 22:17:28 +0300

    now returns array of hashes with all data

--- aero_crawler.rb
+++ aero_crawler.rb
@@ -1,4 +1,4 @@
-require 'open-uri'
+﻿require 'open-uri'
 require 'nokogiri'
 require 'watir'
 require 'json'
@@ -9,13 +9,39 @@ browser.goto 'https://www.aeroflot.ru/ru-ru/destination_offers'
 
 doc = Nokogiri::HTML(browser.html)
 
+arr = []
 
-doc.css('.directions__to').each do |item|
+doc.css('.directions__item').each do |item|
 
+from = item.at_css('.directions__from').content.strip
+to = item.at_css('.directions__to').content.strip
+price = item.at_css('.directions__price').content.strip
+tariff = item.at_css('.directions__tariff').content.strip
+date = item.at_css('.directions__date').content.strip
 
-puts item.content
+arr << {
+:from => from,
+:to => to,
+:price => price,
+:tariff => tariff,
+:date => date,
+}
 
 end
+
+arr.each do |item|
+
+puts "#{item[:from]},#{item[:to]},#{item[:price]},#{item[:tariff]},#{item[:date]}"
+
+end
+
+
+#doc.css('.directions__to').each do |item|
+
+
+#puts item.content
+
+#end
 
 
 
